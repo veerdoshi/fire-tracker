f9e745e5622f25a735602511e7768a7a92f968f1
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2018-02-04 10:33:39 -0800

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -3,9 +3,9 @@ from models.item import ItemModel
 from matplotlib import pyplot as plt
 
 class Graphing():
-   while True:
-     item = ItemModel.find_by_measure(name)
-     plt.plot([x+1], [item])   
-     plt.show()
+#   while True:
+#     item = ItemModel.find_by_measure(name)
+   plt.plot([1,2,3,4,5], [5,6,7,3,8])   
+   plt.show()
      
      
