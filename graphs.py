89a710392636911dd13e6506979712556600fd41
Veer Doshi <14866767+veerdoshi@users.noreply.github.com>
2018-02-04 10:14:56 -0800

    Update graphs.py

--- graphs.py
+++ graphs.py
@@ -1,6 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.item import ItemModel
-import matplotlib.pyplot as plt
+from matplotlib import matplotlib.pyplot as plt
 
 class Graphing():
    while True:
