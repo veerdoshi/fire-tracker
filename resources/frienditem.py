7618add043921206a53e931dad9582459a2a8c4f
Veer Doshi <theveerdoshi@gmail.com>
2019-07-10 15:36:17 -0700

    Update frienditem.py

--- resources/frienditem.py
+++ resources/frienditem.py
@@ -51,14 +51,15 @@ class FriendItem(Resource):
     def put(self, phonedigits):
         frienddata = FriendItem.parser.parse_args()
 
-        #y = phonedigits.split("-")
+        y = phonedigits.split("-")
         #frienditem = FriendItemModel.query.filter_by(phonedigits=y[0],friendphone=y[1]).all()
 
-        frienditem = FriendItemModel.find_by_measure(phonedigits)
+        frienditem = FriendItemModel.query.filter_by(phonedigits=y[0], friendphone=y[1]).all()
+        #frienditem = FriendItemModel.find_by_measure(phonedigits)
 
         if frienditem is None:
             #y = phonedigits.split("-")
-            frienditem = FriendItemModel(phonedigits, frienddata['friendname'], frienddata['friendphone'])
+            frienditem = FriendItemModel(y[0], frienddata['friendname'], frienddata['friendphone'])
         else:
             frienditem.friendname = frienddata['friendname']
             frienditem.friendphone = frienddata['friendphone']
