c85825a33cbdc0202a01ee24f7cafc2f77b71010
Veer Doshi <theveerdoshi@gmail.com>
2019-07-10 17:33:01 -0700

    Update item.py

--- resources/item.py
+++ resources/item.py
@@ -34,9 +34,9 @@ class Item(Resource):
         else:
             x = phonenumber.split("+")
             for y in range(0,len(x)):
-                friendsObj['friends'].append([item.json() for item in ItemModel.query.filter_by(phonenumber=x[y]).all()])
+                friendsObj['friends'].append(item.json() for item in ItemModel.query.filter_by(phonenumber=x[y]).all())
                 #friendsObj['friends'].append([item.json() for item in ItemModel.query.filter_by(latitude=x[y]).all()])
-        return {'friends': friendsObj['friends'][0]}
+        return friendsObj
 
         #item = ItemModel.find_by_measure(phonenumber)
         #if item:
